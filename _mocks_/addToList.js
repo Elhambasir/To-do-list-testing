5fb4d1f34529c1c193b4198e5c2b5fba6ae484c3
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-04-06 21:01:52 +0430

    Implement jest testing part 1

---
+++ _mocks_/addToList.js
@@ -0,0 +1,16 @@
+const taskList = [];
+function addTask(item) {
+  taskList.push(item);
+  return taskList;
+}
+
+function removeTask(data) {
+  const filteredData = taskList.filter((item) => item.index !== data.index);
+  for (let i = 0; i < filteredData.length; i += 1) {
+    filteredData[i].index = i + 1;
+  }
+  return filteredData;
+}
+
+exports.addTask = addTask;
+exports.removeTask = removeTask;
---
+++ modules/task.test.js
@@ -0,0 +1,27 @@
+const { addTask, removeTask } = require('../_mocks_/addToList.js');
+
+describe('Add and Remove To-do-list', () => {
+  test('should add items', () => {
+    const result = addTask(
+      {
+        title: 'fake description for testing',
+        index: 1,
+        completed: false,
+      },
+    );
+    expect(result).toStrictEqual([
+      {
+        title: 'fake description for testing',
+        index: 1,
+        completed: false,
+      },
+    ]);
+  });
+  test('delete item', () => {
+    expect(removeTask({
+      title: 'fake description for testing',
+      index: 1,
+      completed: false,
+    })).toStrictEqual([]);
+  });
+});
